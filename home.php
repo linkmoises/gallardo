0b327f2b258e3effee2332acf02d944fd2f94686
Moisés Serrano <linkmoises@gmail.com>
2020-02-07 00:30:06 -0500

    loop añadido

--- home.php
+++ home.php
@@ -103,15 +103,17 @@
 			<section class="recent-posts">
 				<div class="section-title"><h2><span>Todas las historias</span></h2></div>
 				<div class="row listrecent"> <!-- inicio fila recientes -->
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<!-- inicio post -->
-					<div class="col-lg-4 col-md-6 mb-30px card-group">
+					<div id="post-<?php the_ID(); ?>" class="col-lg-4 col-md-6 mb-30px card-group">
 						<div class="card h-100">
 							<div class="maxthumb">
-								<a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x500" alt="Powerful things you can do with the Markdown editor"></a>
+								<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' , array( 'class' => 'img-fluid') ) ; ?></a>
 							</div>
 							<div class="card-body">
-								<h2 class="card-title"><a class="text-dark" href="#">Powerful things you can do with the Markdown editor</a></h2>
-								<h4 class="card-text">There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips...</h4>
+								<h2 class="card-title"><a class="text-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+								<h4 class="card-text"><?php the_excerpt(); ?></h4>
 							</div>
 							<div class="card-footer bg-white">
 								<div class="wrapfooter">
@@ -122,143 +124,15 @@
 										<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
 										<span class="post-date">12 Jun 2018</span>
 									</span>
-									<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
+									<span class="post-read-more"><a href="<?php the_permalink(); ?>" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
 									<div class="clearfix"></div>
 								</div>
 							</div>
 						</div>
 					</div>
 					<!-- end post -->
-					<!-- inicio post -->
-					<div class="col-lg-4 col-md-6 mb-30px card-group">
-						<div class="card h-100">
-							<div class="maxthumb">
-								<a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x500" alt="Powerful things you can do with the Markdown editor"></a>
-							</div>
-							<div class="card-body">
-								<h2 class="card-title"><a class="text-dark" href="#">Powerful things you can do with the Markdown editor</a></h2>
-								<h4 class="card-text">There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips...</h4>
-							</div>
-							<div class="card-footer bg-white">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-										<img class="author-thumb" src="https://0.gravatar.com/avatar/f5b8708b8db2369736d0bbe660cbdd47?s=400&d=mm" alt="Moy">
-									</span>
-									<span class="author-meta">
-										<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
-										<span class="post-date">12 Jun 2018</span>
-									</span>
-									<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-									<div class="clearfix"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- end post -->
-					<!-- inicio post -->
-					<div class="col-lg-4 col-md-6 mb-30px card-group">
-						<div class="card h-100">
-							<div class="maxthumb">
-								<a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x500" alt="Powerful things you can do with the Markdown editor"></a>
-							</div>
-							<div class="card-body">
-								<h2 class="card-title"><a class="text-dark" href="#">Powerful things you can do with the Markdown editor</a></h2>
-								<h4 class="card-text">There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips...</h4>
-							</div>
-							<div class="card-footer bg-white">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-										<img class="author-thumb" src="https://0.gravatar.com/avatar/f5b8708b8db2369736d0bbe660cbdd47?s=400&d=mm" alt="Moy">
-									</span>
-									<span class="author-meta">
-										<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
-										<span class="post-date">12 Jun 2018</span>
-									</span>
-									<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-									<div class="clearfix"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- end post -->
-					<!-- inicio post -->
-					<div class="col-lg-4 col-md-6 mb-30px card-group">
-						<div class="card h-100">
-							<div class="maxthumb">
-								<a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x500" alt="Powerful things you can do with the Markdown editor"></a>
-							</div>
-							<div class="card-body">
-								<h2 class="card-title"><a class="text-dark" href="#">Powerful things you can do with the Markdown editor</a></h2>
-								<h4 class="card-text">There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips...</h4>
-							</div>
-							<div class="card-footer bg-white">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-										<img class="author-thumb" src="https://0.gravatar.com/avatar/f5b8708b8db2369736d0bbe660cbdd47?s=400&d=mm" alt="Moy">
-									</span>
-									<span class="author-meta">
-										<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
-										<span class="post-date">12 Jun 2018</span>
-									</span>
-									<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-									<div class="clearfix"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- end post -->
-					<!-- inicio post -->
-					<div class="col-lg-4 col-md-6 mb-30px card-group">
-						<div class="card h-100">
-							<div class="maxthumb">
-								<a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x500" alt="Powerful things you can do with the Markdown editor"></a>
-							</div>
-							<div class="card-body">
-								<h2 class="card-title"><a class="text-dark" href="#">Powerful things you can do with the Markdown editor</a></h2>
-								<h4 class="card-text">There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips...</h4>
-							</div>
-							<div class="card-footer bg-white">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-										<img class="author-thumb" src="https://0.gravatar.com/avatar/f5b8708b8db2369736d0bbe660cbdd47?s=400&d=mm" alt="Moy">
-									</span>
-									<span class="author-meta">
-										<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
-										<span class="post-date">12 Jun 2018</span>
-									</span>
-									<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-									<div class="clearfix"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- end post -->
-					<!-- inicio post -->
-					<div class="col-lg-4 col-md-6 mb-30px card-group">
-						<div class="card h-100">
-							<div class="maxthumb">
-								<a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x500" alt="Powerful things you can do with the Markdown editor"></a>
-							</div>
-							<div class="card-body">
-								<h2 class="card-title"><a class="text-dark" href="#">Powerful things you can do with the Markdown editor</a></h2>
-								<h4 class="card-text">There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips...</h4>
-							</div>
-							<div class="card-footer bg-white">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-										<img class="author-thumb" src="https://0.gravatar.com/avatar/f5b8708b8db2369736d0bbe660cbdd47?s=400&d=mm" alt="Moy">
-									</span>
-									<span class="author-meta">
-										<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
-										<span class="post-date">12 Jun 2018</span>
-									</span>
-									<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-									<div class="clearfix"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- end post -->
+<?php endwhile; ?>
+<?php endif; ?>
 				</div> <!-- fin fila recientes -->
 			</section>
 
