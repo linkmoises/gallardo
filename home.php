2cd878b4f86a8f78b35809adf3cca69b2cdc06bd
Moisés Serrano <linkmoises@gmail.com>
2020-02-13 19:39:36 -0500

    añadido destacados funcionando con categoría destacados y eliminada función que usaba meta_key para mostrar destacados

--- home.php
+++ home.php
@@ -18,11 +18,24 @@
 ================================================== -->
 		<div class="main-content">
 
+<?php
+$featured_loop = new WP_Query(
+	array(
+		'post_type' => 'post',
+		'posts_per_page' => 2,
+		'category_name' => 'destacado',
+		//'order' => 'ASC',
+		//'orderby' => 'name'
+	)
+);
+?>
+<?php if ( $featured_loop->have_posts() ): ?>
 <!-- Posts Destacados
 ================================================== -->
 			<section class="featured-posts">
 				<div class="section-title"><h2><span>Más visitados</span></h2></div>
 				<div class="row"><!-- inicio fila -->
+<?php while ( $featured_loop->have_posts() ) : $featured_loop->the_post(); ?>
 					<!-- inicio post -->
 					<div class="col-md-6 mb-30px">
 						<div class="listfeaturedtag h-100">
@@ -38,7 +51,7 @@
 									<div class="h-100 card-group">
 										<div class="card">
 											<div class="card-body">
-												<h2 class="card-title"><a class="text-dark" href="#">Let's test spoilers</a></h2>
+												<h2 class="card-title"><?php the_title(); ?></h2>
 												<h4 class="card-text">Director Roland Suso Richter’s enigmatic psychological thriller (direct to video/DVD) was based upon screenwriter Michael Cooney’s own play “Point of Death” - a title that...</h4>
 											</div>
 											<div class="card-footer b-0 bg-white mt-auto">
@@ -59,62 +72,30 @@
 						</div>
 					</div>
 					<!-- fin post -->
-					<!-- inicio post -->
-					<div class="col-md-6 mb-30px">
-						<div class="listfeaturedtag h-100">
-							<div class="row h-100">
-								<div class="col-12 col-md-12 col-lg-5 pr-lg-0">
-									<div class="h-100">
-										<div class="wrapthumbnail">
-											<a href="#"><img class="featured-box-img-cover" src="http://via.placeholder.com/750x500"></a>
-										</div>
-									</div>
-								</div>
-								<div class="col-12 col-md-12 col-lg-7">
-									<div class="h-100 card-group">
-										<div class="card">
-											<div class="card-body">
-												<h2 class="card-title"><a class="text-dark" href="#">Let's test spoilers</a></h2>
-												<h4 class="card-text">Director Roland Suso Richter’s enigmatic psychological thriller (direct to video/DVD) was based upon screenwriter Michael Cooney’s own play “Point of Death” - a title that...</h4>
-											</div>
-											<div class="card-footer b-0 bg-white mt-auto">
-												<div class="wrapfooter">
-													<span class="meta-footer-thumb"><img class="author-thumb" src="https://0.gravatar.com/avatar/f5b8708b8db2369736d0bbe660cbdd47?s=400&d=mm" alt="Moy"></span>
-													<span class="author-meta">
-														<span class="post-name"><a target="_blank" href="#">Moy</a></span><br/>
-														<span class="post-date">11 Jan 2018</span>
-													</span>
-													<span class="post-read-more"><a href="#" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-													<div class="clearfix"></div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- fin post -->
+<?php endwhile; ?>
 				</div> <!-- fin fila -->
 			</section>
-
-<!-- Todos los posts
-================================================== -->
-			<section class="recent-posts">
-				<div class="section-title"><h2><span>Todas las historias</span></h2></div>
-				<div class="row listrecent"> <!-- inicio fila recientes -->
+<?php endif; ?>
+<?php wp_reset_query(); ?>
 
 <?php
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 $home_loop = new WP_Query(
 	array(
 		'post_type' => 'post',
 		'posts_per_page' => 6,
+		'paged' => $paged,
 		//'order' => 'ASC',
 		//'orderby' => 'name'
 	)
 );
 ?>
 <?php if ( $home_loop->have_posts() ) { ?>
+<!-- Todos los posts
+================================================== -->
+			<section class="recent-posts">
+				<div class="section-title"><h2><span>Todas las historias</span></h2></div>
+				<div class="row listrecent"> <!-- inicio fila recientes -->
 <?php while ( $home_loop->have_posts() ) : $home_loop->the_post(); ?>
 					<!-- inicio post -->
 					<div id="post-<?php the_ID(); ?>" class="col-lg-4 col-md-6 mb-30px card-group">
@@ -145,7 +126,6 @@ $home_loop = new WP_Query(
 					</div>
 					<!-- end post -->
 <?php endwhile; ?>
-<?php } ?>
 				</div> <!-- fin fila recientes -->
 			</section>
 
@@ -159,6 +139,7 @@ $home_loop = new WP_Query(
 			</div>
 <!-- Fin Paginación
 ================================================== -->
+<?php } ?>
 
 		</div><!-- cierra .main-content -->
 	</div> <!-- cierra .container -->
