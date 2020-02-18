a35ef29c3de30bcdd0ee9ec598d7a6a352fc6bcf
Moisés Serrano <linkmoises@gmail.com>
2020-02-18 13:22:42 -0500

    correción menor

--- single.php
+++ single.php
@@ -67,13 +67,13 @@
 			<hr class="inter-meta" />
 			<!-- Inicio Meta -->
 			<div class="row post-top-meta">
-				<div class="col-4 col-sm-3 col-md-2">
-					<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 72 ); ?></a>
+				<div class="col-4 col-sm-3 col-md-2 text-center">
+					<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 90 ); ?></a>
 				</div>
 				<div class="col-8 col-sm-9 col-md-10">
-					<a class="link-dark" href="#">Moisés Serrano Samudio</a><a href="#" class="btn follow"><i class="far fa-address-card"></i> Follow</a>
+					<a class="link-dark" href="#">Moisés Serrano Samudio</a><a href="#" class="btn follow"><i class="fab fa-twitter"></i> Seguir</a>
 					<span class="author-description d-none d-lg-block">Médico de atención primaria, fotógrafo aficionado, apasionado de las tecnologías relacionadas con el EdTech y el eHealth y diseñador/desarrollador de sitios web de salud.</span>
-					<span class="author-description d-lg-none">Médico de atención primaria, apasionado del EdTech y el eHealth y diseñador/desarrollador de sitios web de salud.</span>
+					<span class="author-description d-lg-none">Médico, apasionado del EdTech/eHealth y diseñador/desarrollador de sitios web de salud.</span>
 				</div>
 			</div>
 			<!-- Fin Meta -->
