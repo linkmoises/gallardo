6c1c991eb13b2779c2235a913fca604572fc8320
Moisés Serrano <linkmoises@gmail.com>
2022-04-24 12:48:43 -0500

    Otra correción menor

--- include/related-posts.php
+++ include/related-posts.php
@@ -25,11 +25,11 @@
 					<div class="card-footer bg-white">
 						<div class="wrapfooter">
 							<span class="meta-footer-thumb">
-								<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 72 ); ?></a>
+								<a href="https://moisesserrano.com/sobre-mi/"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '48', $default, $alt, array( 'class' => array( 'rounded-circle', 'mr-2' ) ) ); ?></a>
 							</span>
 							<span class="author-meta">
 								<span class="post-name"><a target="_blank" href="#"><?php the_author(); ?></a></span><br/>
-								<span class="post-date"><?php echo get_the_date('j M Y'); ?></span> - 
+								<span class="post-date"><?php echo get_the_date('j M Y'); ?></span> -
 								<span class="post-read"><?php echo gallo_reading_time(); ?></span>
 							</span>
 							<span class="post-read-more"><a href="<?php the_permalink(); ?>" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
