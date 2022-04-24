8671ef55dc12f4c2896abc434f5c6e846eb268fb
Moisés Serrano <linkmoises@gmail.com>
2022-04-24 01:37:14 -0500

    Cambiados tamaños de avatares

--- home.php
+++ home.php
@@ -56,19 +56,19 @@ $featured_loop = new WP_Query(
 												<h4 class="card-text"><?php the_excerpt(); ?></h4>
 											</div>
 											<div class="card-footer b-0 bg-white mt-auto">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-										<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 72 ); ?></a>
-									</span>
-									<span class="author-meta">
-										<span class="post-name"><a target="_blank" href="#"><?php the_author(); ?></a></span><br/>
-										<span class="post-date"><?php echo get_the_date('j M Y'); ?></span>
-										<span class="dot"></span>
-										<span class="post-read"><?php echo gallo_reading_time(); ?></span>
-									</span>
-									<span class="post-read-more"><a href="<?php the_permalink(); ?>" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
-									<div class="clearfix"></div>
-								</div>
+												<div class="wrapfooter">
+													<span class="meta-footer-thumb">
+														<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 40 ); ?></a>
+													</span>
+													<span class="author-meta">
+														<span class="post-name"><a target="_blank" href="#"><?php the_author(); ?></a></span><br/>
+														<span class="post-date"><?php echo get_the_date('j M Y'); ?></span>
+														<span class="dot"></span>
+														<span class="post-read"><?php echo gallo_reading_time(); ?></span>
+													</span>
+													<span class="post-read-more"><a href="<?php the_permalink(); ?>" title="Leer historia"><i class="fas fa-bookmark"></i></a></span>
+													<div class="clearfix"></div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -116,7 +116,7 @@ $home_loop = new WP_Query(
 							<div class="card-footer bg-white">
 								<div class="wrapfooter">
 									<span class="meta-footer-thumb">
-										<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 72 ); ?></a>
+										<a href="#"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 40 ); ?></a>
 									</span>
 									<span class="author-meta">
 										<span class="post-name"><a target="_blank" href="#"><?php the_author(); ?></a></span><br/>
